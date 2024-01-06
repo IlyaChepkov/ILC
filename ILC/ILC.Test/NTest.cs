8c28d312c9d5b6664f4dc74b0a0f9d8031d565c7
Chepkov Igor <ichepkov@gmail.com>
2024-01-06 18:08:04 +0300

    Больше тестов на N

--- ILC/ILC.Test/NTest.cs
+++ ILC/ILC.Test/NTest.cs
@@ -6,7 +6,7 @@ namespace ILC.Test
         [TestMethod]
         public void BuildOuput()
         {
-            for (int i = 0; i < 1000; i++)
+            for (int i = 0; i <= 1000; i++)
             {
                 string s = i.ToString();
                 N number = new N(s);
@@ -17,7 +17,7 @@ namespace ILC.Test
         [TestMethod]
         public void Clone()
         {
-            for (int i = 0; i < 1000; i++)
+            for (int i = 0; i <= 1000; i++)
             {
                 string s = i.ToString();
                 N number = new N(s);
@@ -29,9 +29,9 @@ namespace ILC.Test
         [TestMethod]
         public void Compare()
         {
-            for (int i = 0; i < 1000; i++)
+            for (int i = 0; i <= 1000; i++)
             {
-                for (int j = 0; j < 1000; j++)
+                for (int j = 0; j <= 1000; j++)
                 {
                     N first = new N(i.ToString());
                     N second = new N(j.ToString());
@@ -44,7 +44,7 @@ namespace ILC.Test
         [TestMethod]
         public void IsZero()
         {
-            for (int i = 0; i < 1000; i++)
+            for (int i = 0; i <= 1000; i++)
             {
                 N value = new N(i.ToString());
                 bool res = value.IsZero();
@@ -55,11 +55,175 @@ namespace ILC.Test
         [TestMethod]
         public void Increment()
         {
-            for (int i = 0; i < 1000; i++)
+            for (int i = 0; i <= 1000; i++)
             {
                 N value = new N(i.ToString());
                 value++;
-                Assert.AreEqual((i + 1).ToString(), value);
+                Assert.AreEqual((i + 1).ToString(), value.ToString());
+            }
+        }
+
+        [TestMethod]
+        public void Plus()
+        {
+            for (int i = 0; i <= 1000; i++)
+            {
+                for (int j = 0; j <= 1000; j++)
+                {
+                    N first = new N(i.ToString());
+                    N second = new N(j.ToString());
+                    Assert.AreEqual((i + j).ToString(), (first + second).ToString());
+                }
+            }
+        }
+
+        [TestMethod]
+        public void Minus()
+        {
+            for (int i = 0; i <= 1000; i++)
+            {
+                for (int j = 0; j <= 1000; j++)
+                {
+                    N first = new N(i.ToString());
+                    N second = new N(j.ToString());
+                    Assert.AreEqual(System.Math.Abs(i - j).ToString(), (first - second).ToString());
+                }
+            }
+        }
+
+        [TestMethod]
+        public void MulDighit()
+        {
+            for (int i = 0; i <= 1000; i++)
+            {
+                for (byte j = 0; j <= 9; j++)
+                {
+                    N first = new N(i.ToString());
+                    Assert.AreEqual((i * j).ToString(), (first.MulDigit(j)).ToString());
+                }
+            }
+        }
+
+        [TestMethod]
+        public void MulTen()
+        {
+            for (int i = 0; i <= 1000; i++)
+            {
+                for (byte j = 1; j <= 9; j++)
+                {
+                    N first = new N(i.ToString());
+                    Assert.AreEqual((i * System.Math.Pow(10, j)).ToString(), (first).ToString());
+                }
+            }
+        }
+
+        [TestMethod]
+        public void Mul()
+        {
+            for (int i = 0; i <= 1000; i++)
+            {
+                for (int j = 0; j <= 1000; j++)
+                {
+                    N first = new N(i.ToString());
+                    N second = new N(j.ToString());
+                    Assert.AreEqual((i * j).ToString(), (first/* * second*/).ToString());
+                }
+            }
+        }
+
+        [TestMethod]
+        public void SubMullable()
+        {
+            for (int i = 0; i <= 1000; i++)
+            {
+                for (int j = 0; j <= 1000; j++)
+                {
+                    for (byte k = 0; k <= 9; k++)
+                    {
+                        if (i >= j * k)
+                        {
+                            N first = new N(i.ToString());
+                            N second = new N(j.ToString());
+                            Assert.AreEqual((i - j * k).ToString(), (first/* * second*/).ToString());
+                        }
+                    }
+                }
+            }
+        }
+
+        [TestMethod]
+        public void FirstDivid()
+        {
+            for (int i = 0; i <= 1000; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    N first = new N(i.ToString());
+                    N second = new N(j.ToString());
+                    Assert.AreEqual((i / j).ToString()[0].ToString(), (first/* * second*/).ToString());
+                }
+            }
+        }
+
+        [TestMethod]
+        public void Div()
+        {
+            for (int i = 0; i <= 1000; i++)
+            {
+                for (int j = 1; j <= 1000; j++)
+                {
+                    N first = new N(i.ToString());
+                    N second = new N(j.ToString());
+                    Assert.AreEqual((i / j).ToString(), (first/* * second*/).ToString());
+                }
+            }
+        }
+
+        [TestMethod]
+        public void Mod()
+        {
+            for (int i = 0; i <= 1000; i++)
+            {
+                for (int j = 1; j <= 1000; j++)
+                {
+                    N first = new N(i.ToString());
+                    N second = new N(j.ToString());
+                    Assert.AreEqual((i % j).ToString(), (first/* * second*/).ToString());
+                }
+            }
+        }
+
+        [TestMethod]
+        public void GCF ()
+        {
+            for (int i = 0; i <= 1000; i++)
+            {
+                for (int j = 0; j <= 1000; j++)
+                {
+                    if (i != j || i != 0)
+                    {
+                        N first = new N(i.ToString());
+                        N second = new N(j.ToString());
+                        Assert.AreEqual((i % j).ToString(), (first/* * second*/).ToString());
+                    }
+                }
+            }
+        }
+
+        [TestMethod]
+        public void LCM()
+        {
+            for (int i = 0; i <= 1000; i++)
+            {
+                for (int j = 0; j <= 1000; j++)
+                {
+                    if (i != j || i != 0)
+                    {
+                        N first = new N(i.ToString());
+                        N second = new N(j.ToString());
+                        Assert.AreEqual((i % j).ToString(), (first/* * second*/).ToString());
+                    }
+                }
             }
         }
     }
