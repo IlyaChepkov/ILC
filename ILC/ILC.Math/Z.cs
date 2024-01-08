0470fc09ab3be496d25633c43bf08b19ac647dd6
IlyaChepkov <ilya.chepkov2011@gmail.com>
2024-01-08 16:11:19 +0300

    Z create

--- ILC/ILC.Math/Z.cs
+++ ILC/ILC.Math/Z.cs
@@ -6,7 +6,70 @@ using System.Threading.Tasks;
 
 namespace ILC.Math
 {
-    internal class Z
+    public class Z
     {
+        private N value;
+        private bool isPositive;
+
+        public Z(string input)
+        {
+            if (input[0] == '-')
+            {
+                isPositive = false;
+                input = input.Remove(0, 1);
+            }
+            else isPositive = true;
+            value = new N(input);
+        }
+
+        public Z Abs()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte IsPositive()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Z ChangeSign()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static implicit operator N(Z param)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static explicit operator Z(N param)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Z operator +(Z first, Z second)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Z operator -(Z first, Z second)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Z operator *(Z first, Z second)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Z operator /(Z first, Z second)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Z operator %(Z first, Z second)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
