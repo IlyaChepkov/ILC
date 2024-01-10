946c3b325b466a122da2087d7e27ed8787aab524
IlyaChepkov <ilya.chepkov2011@gmail.com>
2024-01-10 18:19:04 +0300

    Commit

--- ILC/ILC.Math/Z.cs
+++ ILC/ILC.Math/Z.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ILC.Math
+﻿namespace ILC.Math
 {
     public class Z
     {
@@ -13,6 +7,11 @@ namespace ILC.Math
 
         public Z(string input)
         {
+            if (input =="-0")
+            {
+                isPositive = true;
+                value = new N(input);
+            }
             if (input[0] == '-')
             {
                 isPositive = false;
@@ -22,44 +21,91 @@ namespace ILC.Math
             value = new N(input);
         }
 
-        public override string ToString() => throw new NotImplementedException();
+        private Z(N value, bool isPositive)
+        {
+            this.value = value;
+            this.isPositive = isPositive;
+        }
 
-        public Z Clone() => throw new NotImplementedException();
+        public override string ToString() =>
+            isPositive == false ? "-" + value.ToString() : "" + value.ToString();
 
-        public static byte Compare(Z first, Z second) => throw new NotImplementedException();
+        public Z Clone() => new Z(value, isPositive);
 
-        public bool IsZero() => throw new NotImplementedException();
+        public static byte Compare(Z first, Z second)
+        {
+            if (first.isPositive == second.isPositive && N.Compare(first.value, second.value) == 0)
+                return 0;
+            if (first.isPositive && !second.isPositive)
+                return 2;
+            else if (!first.isPositive && second.isPositive)
+                return 1;
+            else
+            {
+                if (!first.isPositive)
+                {
+                    if (N.Compare(first.value, second.value) == 1)
+                        return 2;
+                    else
+                        return 1;
+                }
+                else
+                {
+                    return N.Compare(first.value, second.value);
+                }
+            }
+        }
+
+        public bool IsZero() => value.IsZero();
 
 
         public Z Abs()
         {
-            throw new NotImplementedException();
+            Z result = Clone();
+            result.isPositive = true;
+            return result;
         }
 
         public byte IsPositive()
         {
-            throw new NotImplementedException();
+            if (isPositive)
+                return IsZero() ? (byte)0 : (byte)2;
+            else
+                return 1;
         }
 
         public Z ChangeSign()
         {
-            throw new NotImplementedException();
+            Z result = Clone();
+            if (result.value.ToString() == "0")
+                return result;
+            if (result.isPositive)
+                result.isPositive = false;
+            else
+                result.isPositive = true;
+            return result;
         }
         public static Z GCF(Z first, Z second)
         {
-            throw new NotImplementedException();
+            Z result = N.GCF(first.value, second.value);
+            return result;
         }
 
-        public static N LCM(N first, N second) => throw new NotImplementedException();
+        public static Z LCM(Z first, Z second)
+        {
+            return N.LCM(first.value, second.value);
+        }
 
         public static explicit operator N(Z param)
         {
-            throw new NotImplementedException();
+            if (!param.isPositive)
+                throw new ArgumentException();
+            return param.value;
         }
 
         public static implicit operator Z(N param)
         {
-            throw new NotImplementedException();
+            return new Z(param.ToString());
         }
 
         public static Z operator +(Z first, Z second)
